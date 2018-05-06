458a85f1784e183d746191cae2ccbb151d919194
Erich Rodrigues Ferrares <erich.rodrigues@yahoo.com.br>
2018-05-06 14:15:17 -0300

    mas nao era so eu

--- base/static/js/poller.js
+++ base/static/js/poller.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
     for (var i = 1; i <= count; i++) {
       var el = posts[posts.length - i];
       el.parentNode.removeChild(el);
-      $grid.isotope('remove', $(el);
+      $grid.isotope('remove', $(el));
     }
   }
 
