a56483ed82cbd2f6fe27e7ff2a62931fc9c9b705
Gustavo Guimarães <gustavoguimaraes18@gmail.com>
2018-05-06 11:49:16 -0300

    vraaaaau

--- base/static/js/main.js
+++ base/static/js/main.js
@@ -1,1 +1,21 @@
+$("body").click(function(event) {
+  var container = $(".modal");
+  if (!container.is(event.target) && container.has(event.target).length === 0) {
+    container.closest(".modal-wrapper").addClass("closed");
+  }
+});
 
+$(".open-modal").click(function(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  $($(this).attr('href')).removeClass("closed");
+  const content = $(this).html();
+  console.log(content);
+  $('.js-modal-content').html(content);
+});
+
+$(".modal-close").click(function(event) {
+  event.preventDefault();
+  var container = $(this).closest(".modal");
+  $(this).closest(".modal-wrapper").addClass("closed");
+});
